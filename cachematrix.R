b449f462ec5cb231cc0874f2120a31d9fad05a1d
lushacao <lushacao@gmail.com>
2019-01-03 15:42:50 -0500

    complete functions
    complete the two functions

--- cachematrix.R
+++ cachematrix.R
@@ -3,8 +3,32 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+## The makeCacheMatrix function take an argument x as the input matrix, object 
+## mi to cache the inverse matrix. 
+## The set() assigns the input argument to the x object in the parent 
+## environment, and assigns the value of NULL to the mi object in the parent 
+## environment. 
+## The get() returns the input matrix.
+## The setInverse() assigns the input argument to the value of mi in the parent 
+## environment.
+## The getInverse() returns the inverse matrix.
+## The list() returns a fully formed object of type makeCacheMatrix() to be used 
+## by downstream R code.
 
+makeCacheMatrix <- function(x = matrix()) {
+        mi <- NULL
+        set <- function(y) {
+                x <<- y
+                mi <<- NULL
+        }
+        get <- function() x
+        setInverse <- function(inverse) {
+                mi <<- inverse
+        }
+        getInverse <- function() mi
+        list(set = set, get = get,
+             setInverse = setInverse,
+             getInverse = getInverse)
 }
 
 
@@ -12,4 +36,14 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        ## Return a matrix that is the inverse of 'x'
+        mi <- x$getInverse()
+        if(!is.null(mi)) {
+                message("getting cached data")
+                return(mi)
+        }
+        data <- x$get()
+        mi <- solve(data)
+        x$setInverse(mi)
+        mi
 }
